2f6939b8376e756f800a26148eebbc85963e17ef
Cody Walenciak <codywalenciak@gmail.com>
2025-03-22 17:28:07 -0500

    Placeholder favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ type Props = {
 export default function RootLayout({ children }: Props) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="favicon.ico" />
+            </head>
             <body className={`antialiased  bg-[#00003c] text-white min-h-screen`}>
                 {children}
             </body>
