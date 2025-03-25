b97cd6c1f31bd24f2f263d1ac1b9e80bc87bbe86
Cody Walenciak <codywalenciak@gmail.com>
2025-03-25 11:19:21 -0500

    Implement auth0 authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { signIn } from "@/auth"
 export default function Page() {
     return (
         <div>
             Everything seems to be working
+            <form
+                action={async () => {
+                    "use server"
+                    await signIn("auth0");
+                }}>
+                <button type="submit">Sign in with Auth0</button>
+            </form>
         </div>
     )
 }
--- auth.ts
+++ auth.ts
@@ -1,17 +1,18 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Auth0 from "next-auth/providers/auth0";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
-    brandColor: "#1ED2AF",
+    brandColor: "#1F6A3E",
     logo: "/logo.png",
-    buttonText: "#ffffff",
+    buttonText: "#FC9E40",
   },
-  providers: [GitHub],
-  callbacks: {
-    authorized: async ({ auth }) => {
-      // Logged in users are authenticated, otherwise redirect to login page
-      return !!auth;
-    },
-  },
+  providers: [Auth0],
+  // callbacks: {
+  //   authorized: async ({ auth }) => {
+  //     // Logged in users are authenticated, otherwise redirect to login page
+  //     return !!auth;
+  //   },
+  //},
 });
