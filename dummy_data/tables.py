e1a0ee1c3544645142d745ca069d5e1c3c61fb5c
Ace <acequantumbit@gmail.com>
2025-03-31 20:15:12 -0500

    idk bro I'm tired

--- dummy_data/tables.py
+++ dummy_data/tables.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 fake = Faker()
 
 # If you're Sean
-DB_PATH = r"C:/Users/seana/OneDrive/Documents/Learning_Playground/Time_data.db"
+# DB_PATH = r"C:/Users/seana/OneDrive/Documents/Learning_Playground/Time_data.db"
 
 # If you're Ace
 DB_PATH = r"/home/acequantum/playtime/dummy_data/1_dummy_data.db"
