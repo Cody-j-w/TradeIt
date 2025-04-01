eaf54e352d537935e2fe6db2580dbecef172b7de
Ace <acequantumbit@gmail.com>
2025-04-01 15:29:55 -0500

    added function to pull tags from text

---
+++ ML_python_scripts/generate_post_embeddings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# I have no idea what a postgres implimentation looks like
+# So I don't know if we need a shebang
+# Anyway here's some imports
+
+from sentence_transformers import SentenceTransformer
+import json
+import re
+
