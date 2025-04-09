3dcc25ce88de30c7662cf04bdc7b30910b3262e7
Cody Walenciak <codywalenciak@gmail.com>
2025-04-09 14:11:36 -0500

    Add userLogin to Redirect component

--- components/Redirect.tsx
+++ components/Redirect.tsx
@@ -1,11 +1,20 @@
 "use client";
+import { userLogin } from "@/lib/data";
+import { getSession } from "@/lib/functions";
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Redirect({ url }: { url: string }) {
     useEffect(() => {
+        const session = async () => {
+            const session = await getSession();
+            if (session && session.user.email) {
+                userLogin(session?.user.email);
+            }
+        }
+        session();
         redirect(url);
-    });
+    }, []);
     return (
         <div className="redirecting">
             Redirecting...
