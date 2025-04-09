6d98ec9660e48062cfc218b87e31e7b21bc646ae
Cody Walenciak <codywalenciak@gmail.com>
2025-04-09 14:18:11 -0500

    Add extra auth check in Redirect

--- components/Redirect.tsx
+++ components/Redirect.tsx
@@ -2,18 +2,24 @@
 import { userLogin } from "@/lib/data";
 import { getSession } from "@/lib/functions";
 import { redirect } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Redirect({ url }: { url: string }) {
+    const [auth, setAuth] = useState(false);
     useEffect(() => {
         const session = async () => {
             const session = await getSession();
             if (session && session.user.email) {
                 userLogin(session?.user.email);
+                setAuth(true);
             }
         }
         session();
-        redirect(url);
+        if (auth === true) {
+            redirect(url);
+        } else {
+            redirect("/");
+        }
     }, []);
     return (
         <div className="redirecting">
