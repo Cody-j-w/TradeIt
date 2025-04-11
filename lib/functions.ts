0706ad0e7b24bd51d6ea487bcc7d038d95edc299
Cody Walenciak <codywalenciak@gmail.com>
2025-04-11 15:20:18 -0500

    Add addPost server action

--- lib/functions.ts
+++ lib/functions.ts
@@ -1,6 +1,7 @@
 "use server";
 import { auth0 } from "./auth0";
-import { userLogin } from "./data";
+import { getSelf, insertPost, userLogin } from "./data";
+import { put } from '@vercel/blob';
 
 export async function getSession() {
     const session = await auth0.getSession();
@@ -10,4 +11,21 @@ export async function getSession() {
 export async function login(name: string, image: string) {
     const loginInfo = await userLogin(name, image);
     return loginInfo;
+}
+
+export async function addPost(data: FormData) {
+    const user = (await getSelf()).id;
+    const postText = data.get("post")?.toString();
+    const goodName = data.get("good")?.toString();
+    const image = data.get("image") as File;
+    const imageUrl = (await imageUpload(image)).url;
+    return await insertPost(user, postText!!, goodName!!, imageUrl);
+}
+
+export async function imageUpload(image: File) {
+    const blob = await put(image.name, image, {
+        access: 'public',
+        addRandomSuffix: true
+    });
+    return blob;
 }
