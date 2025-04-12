f5e789176bea101a3a0c1e0212416e3d7563980c
Ace <acequantumbit@gmail.com>
2025-04-12 17:37:26 -0500

    ok now we try

--- ML_python_scripts/generate_post_embeddings copy.py
+++ ML_python_scripts/generate_post_embeddings copy.py
@@ -5,7 +5,7 @@
 # Anyway here's some imports
 
 # import psycopg2.pool
-# from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer
 # import json
 
 # I imagine that since this'll be running on cody's machine
@@ -25,7 +25,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('all-MiniLM-L6-v2')
 load_dotenv()
 
 
@@ -51,33 +51,33 @@ def preprocess_tags(tags):
     return []
 
 
-# def generate_weighted_embeddings(post):
-#     print(f"generating embeddings for post id {post['id']}")
+def generate_weighted_embeddings(post):
+    print(f"generating embeddings for post id {post['id']}")
 
-#     tags = pull_tags(post['text'])
-#     tags = preprocess_tags(tags)
-#     print(f"tags pulled: {tags}")
+    tags = pull_tags(post['text'])
+    tags = preprocess_tags(tags)
+    print(f"tags pulled: {tags}")
 
-#     text = clean_text(post['text'])
-#     print(f"text pulled: {text}")
+    text = clean_text(post['text'])
+    print(f"text pulled: {text}")
 
-#     goods = post['goods']
-#     print(f"goods pulled: {goods}")
+    goods = post['goods']
+    print(f"goods pulled: {goods}")
 
-#     tag_embedding = model.encode(" ".join(tags)) if tags else np.zeros(384)
-#     goods_embedding = model.encode(goods) * 2
-#     # *2 so we put more weight on the goods that the user wants
-#     # This can be changed later, hyperparameter tuning is vibes based
-#     text_embedding = model.encode(text) if text else np.zeros(384)
+    tag_embedding = model.encode(" ".join(tags)) if tags else np.zeros(384)
+    goods_embedding = model.encode(goods) * 2
+    # *2 so we put more weight on the goods that the user wants
+    # This can be changed later, hyperparameter tuning is vibes based
+    text_embedding = model.encode(text) if text else np.zeros(384)
 
-#     combined = np.concatenate([
-#         tag_embedding,
-#         goods_embedding,
-#         text_embedding * 0.3
-#         # more vibes based hyperparameter tuning
-#     ])
+    combined = np.concatenate([
+        tag_embedding,
+        goods_embedding,
+        text_embedding * 0.3
+        # more vibes based hyperparameter tuning
+    ])
 
-#     return combined
+    return combined
 
 
 @app.get("/api/py/embed")
@@ -99,30 +99,30 @@ def update_post_embeddings():
     print("Data Connection Info:")
     print(os.getenv("DATABASE_URL"))
 
-    # cursor = conn.cursor
+    cursor = conn.cursor
 
-    # cursor.execute(post_query)
-    # posts = cursor.fetchall()
+    cursor.execute(post_query)
+    posts = cursor.fetchall()
 
-    # if not posts:
-        # print("All posts have embeddings")
-        # return
+    if not posts:
+        print("All posts have embeddings")
+        return
 
     update_count = 0
 
-    # for post in posts:
-    #     embedding = generate_weighted_embeddings(post)
-    #     cursor.execute("""
-    #         UPDATE posts
-    #         SET embedding = %s
-    #         WHERE id = %s;
-    #     """, (embedding.tolist(), post['id']))
-    #     # Not sure if it needs to be tolist()
-    #     # Since the datatype is a vector.
+    for post in posts:
+        embedding = generate_weighted_embeddings(post)
+        cursor.execute("""
+            UPDATE posts
+            SET embedding = %s
+            WHERE id = %s;
+        """, (embedding.tolist(), post['id']))
+        # Not sure if it needs to be tolist()
+        # Since the datatype is a vector.
 
-        # update_count += 1
+        update_count += 1
 
-    # conn.commit()
+    conn.commit()
     conn.close()
     print(f"{update_count} posts updated succesfully.")
 
