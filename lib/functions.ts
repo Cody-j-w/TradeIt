83794a23b1b2bead9a41b858190b58f8210d9cad
Cody Walenciak <codywalenciak@gmail.com>
2025-04-13 15:13:38 -0500

    Add getMe(), profile update functions, and follower functions

--- lib/functions.ts
+++ lib/functions.ts
@@ -1,11 +1,18 @@
 "use server";
+import { getMaxAge } from "next/dist/server/image-optimizer";
 import { auth0 } from "./auth0";
-import { getSelf, getSingleUser, getUsers, insertPost, userLogin } from "./data";
+import { fetchFollowedPosts, fetchFollows, getSelf, getSingleUser, getUsers, insertPost, updateAvatar, updateUsername, userLogin } from "./data";
 import { put } from '@vercel/blob';
+import { revalidatePath } from "next/cache";
 
 export async function getSession() {
     const session = await auth0.getSession();
     return session;
+}
+
+export async function getMe() {
+    const me = await getSelf();
+    return me;
 }
 
 export async function login(name: string, image: string) {
@@ -32,10 +39,63 @@ export async function imageUpload(image: File) {
 
 export async function getUser(userEmail: string) {
     const user = await getSingleUser(userEmail);
-    return user;
+    if (user) {
+        return user;
+    } else {
+        return null;
+    }
 }
 
 export async function getAllUsers() {
     const users = await getUsers();
-    return users;
+    if (users) {
+        return users;
+    } else {
+        return null;
+    }
+}
+
+export async function getFollowedPosts() {
+    const follows = await fetchFollowedPosts();
+    if (follows) {
+        return follows;
+    } else {
+        return null;
+    }
+}
+
+export async function getFollowedUsers() {
+    const follows = await fetchFollows();
+    if (follows) {
+        return follows;
+    } else {
+        return null;
+    }
+}
+
+export async function addFollow() {
+
+}
+
+export async function getFollowers() {
+
+}
+
+export async function submitAvatar(data: FormData) {
+    const input = data.get("input") as File;
+    if (input.size > 0) {
+        if (input.type === 'image/jpeg' || input.type === 'image/png') {
+            const uploadedImage = await imageUpload(input);
+            await updateAvatar(uploadedImage.url);
+            revalidatePath("/pages/home");
+        }
+    }
+}
+
+export async function submitUsername(data: FormData) {
+    const input = data.get("input")?.toString();
+    if (input && input.length > 0) {
+        await updateUsername(input);
+        revalidatePath("/pages/home");
+    }
 }
