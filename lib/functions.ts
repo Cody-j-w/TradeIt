fea49e3d36c275c51a34fc7973b9701fec3f29d4
Cody Walenciak <codywalenciak@gmail.com>
2025-04-13 15:54:14 -0500

    Add submitPost function

--- lib/functions.ts
+++ lib/functions.ts
@@ -1,7 +1,7 @@
 "use server";
 import { getMaxAge } from "next/dist/server/image-optimizer";
 import { auth0 } from "./auth0";
-import { fetchFollowedPosts, fetchFollows, getSelf, getSingleUser, getUsers, insertPost, updateAvatar, updateUsername, userLogin } from "./data";
+import { fetchFollowedPosts, fetchFollows, getSelf, getSingleUser, getUsers, insertGood, insertPost, updateAvatar, updateUsername, userLogin } from "./data";
 import { put } from '@vercel/blob';
 import { revalidatePath } from "next/cache";
 
@@ -87,7 +87,7 @@ export async function submitAvatar(data: FormData) {
         if (input.type === 'image/jpeg' || input.type === 'image/png') {
             const uploadedImage = await imageUpload(input);
             await updateAvatar(uploadedImage.url);
-            revalidatePath("/pages/home");
+            revalidatePath("/pages/home"); //placeholder revalidate - change to where ever this function ends up being used, probably /pages/profile
         }
     }
 }
@@ -96,6 +96,31 @@ export async function submitUsername(data: FormData) {
     const input = data.get("input")?.toString();
     if (input && input.length > 0) {
         await updateUsername(input);
-        revalidatePath("/pages/home");
+        revalidatePath("/pages/home"); //placeholder revalidate - change to where ever this function ends up being used, probably /pages/profile
+    }
+}
+
+export async function submitPost(data: FormData) {
+    const user = await getSelf();
+    const text = data.get("text")?.toString();
+    const postGood = data.get("good")?.toString();
+    const image = data.get("image") as File;
+    let imgUrl = undefined;
+    let good = "";
+    if (image.size > 0) {
+        if (image.type === 'image/jpeg' || image.type === 'image/png') {
+            const uploadedImage = await imageUpload(image);
+            imgUrl = uploadedImage.url;
+        }
+    }
+    if (postGood) {
+        const submittedGood = await insertGood(postGood);
+        if (submittedGood) {
+            good = submittedGood?.name
+        }
+    }
+    if (good != "" && text) {
+        const newPost = await insertPost(user.id, text, good, imgUrl);
+        return newPost;
     }
 }
