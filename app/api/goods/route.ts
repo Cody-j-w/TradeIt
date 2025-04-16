806c10f835202879967c12ea58dc6603b037442a
Cody Walenciak <codywalenciak@gmail.com>
2025-04-16 10:58:05 -0500

    Add authentication checks to API endpoints

--- app/api/goods/route.ts
+++ app/api/goods/route.ts
@@ -1,7 +1,12 @@
+import { auth0 } from "@/lib/auth0";
 import { fetchGood, fetchGoods, insertGood } from "@/lib/data";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
+    const auth = await auth0.getSession()
+    if (!auth || !auth?.user) {
+        return NextResponse.json({ "error": "Missing authentication" })
+    }
     const url = new URL(req.url);
     const params = url.searchParams;
     const goodName = params.get('good');
